454d28ae9018968ba46c6c892743a76837611f97
JAMES-ROG\admin <james.dimarco@students.mq.edu.au>
2018-05-16 22:57:31 +1000

    Dashboard names now queried from database

--- browser/urls.py
+++ browser/urls.py
@@ -3,7 +3,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    # path('', views.index, name='index'),
     path('profile/', views.profile, name='profile'),
     path('settings/', views.settings, name='settings'),
     # path('browser/', include('browser.urls')),
--- browser/views.py
+++ browser/views.py
@@ -1,6 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views import generic
+from .models import Dashboard
+
+class IndexView(generic.ListView):
+	template_name = 'browser/home.html'
+
+	def get_queryset(self):
+
+		dash_list = Dashboard.objects.all().order_by("id")
+		
+		i = 0
+
+		row1 = [];	row2 = []; row3 = []; row4 = []; row5 = [];
+		
+		for item in dash_list:
+			if i < 6:
+				row1.append(dash_list[i])
+			elif i < 12:
+				row2.append(dash_list[i])
+			elif i < 18:
+				row3.append(dash_list[i])
+			elif i < 24:
+				row4.append(dash_list[i])
+			elif i < 30:
+				row5.append(dash_list[i])
+			i+=1
+
+		return [row1,row2,row3,row4,row5]
+
+
 def index(request):
 	return render(request, 'browser/home.html')
 
